9b93e61269ee8a0b0e0233f6958165ece8698422
lior2911 <aytegev1@gmail.com>
2022-06-15 08:26:04 +0300

    firstCommit

---
+++ main.js
@@ -0,0 +1,27 @@
+// function sortString(arrayNames){
+//    console.log(arrayNames.sort());
+// }
+// sortString(["lior","eldad","malada","abaineh","solomon"])
+
+// function sortNumbersMinToMax(arrayNums){
+//   console.log(arrayNums.sort((a,b)=>a-b))
+// }
+// sortNumbersMinToMax([21,14,554,76,876,2234,32,9])
+
+// function sortNumMaxToMin(numsArray){
+//   console.log(numsArray.sort((a,b)=>b-a))
+
+// }
+// sortNumMaxToMin([12,43,5453,12,21,55,67,9])
+
+function sortNamesAtoZ(){
+  let inputName = document.createElement("input")
+  document.body.append("inputName");
+  let arrayNames = [];
+  // // if()
+  //   arrayNames.push(inputFName)
+
+  
+
+}
+
